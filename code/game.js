93171806b194554336f18d76c219a67a9c655855
ranaelric <ralrifai@knights.ucf.edu>
2016-10-24 01:37:40 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -61,20 +61,6 @@ function Player(pos) {
 }
 Player.prototype.type = "player";
 
-function Lava(pos, ch) {
-  this.pos = pos;
-  this.size = new Vector(1, 1);
-  if (ch == "=") {
-    this.speed = new Vector(2, 0);
-  } else if (ch == "|") {
-    this.speed = new Vector(0, 2);
-  } else if (ch == "v") {
-    this.speed = new Vector(10, 19);
-    this.repeatPos = pos;
-  }
-}
-Lava.prototype.type = "lava";
-
 // Helper function to easily create an element of a type provided 
 // and assign it a class.
 function elt(name, className) {
