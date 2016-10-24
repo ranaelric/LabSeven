d0860ba5536f7bccbbda3dbe8c3792985087dff0
ranaelric <ralrifai@knights.ucf.edu>
2016-10-24 01:44:36 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -231,6 +231,8 @@ Player.prototype.moveX = function(step, level, keys) {
   var obstacle = level.obstacleAt(newPos, this.size);
   if (obstacle != "wall")
     this.pos = newPos;
+  if (obstacle = "lava")
+    this.pos = (10, 10);
 };
 
 var gravity = 20;
