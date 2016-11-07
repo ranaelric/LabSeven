0ed6e2bc37e63d804a3a2d1c5464737e4144d296
ranaelric <ralrifai@knights.ucf.edu>
2016-11-07 15:16:50 -0500

    Update game.js

--- code/game.js
+++ code/game.js
@@ -75,7 +75,7 @@ Vector.prototype.times = function(factor) {
 // A Player has a size, speed and position.
 function Player(pos) {
   this.pos = pos.plus(new Vector(0, -0.5));
-  this.size = new Vector(0.8, 1.5);
+  this.size = new Vector(3, 3);
   this.speed = new Vector(0, 0);
 }
 Player.prototype.type = "player";
